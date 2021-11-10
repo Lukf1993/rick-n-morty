802cc54a6fbddbb0519aeecd61953a203c6b562b
Lukasz <lfundament@netizens.pl>
2021-11-10 20:24:49 +0100

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/reset.css'
+import '../styles/global.scss'
+import '../styles/breakpoints.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
