0ec8ba546dadd7e224b3723dbc1cba2fd36a60af
Łukasz Fundament <lukasz.fundament@iteo.com>
2022-05-16 11:13:41 +0200

    render user agent

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,10 +40,6 @@ const Home: FC<IProps> = (props) => {
   const [cards, setCards] = useState<ICard[]>(props.data.results)
   const [info, setInfo] = useState<IInfo>(props.data.info)
 
-  useEffect(() => {
-    alert(getUA)
-  }, [])
-
   const getNewCards = async (id:string | null) => {
     if (typeof id !== null) {
       const data:IPropsData = await getPage(id?.charAt(id.length - 1 ))
@@ -55,14 +51,15 @@ const Home: FC<IProps> = (props) => {
   return (
     <>
     <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
-      {cards.map(item => (
-        <Card data={item} key={item.id} />
-      ))}
+      {/*{cards.map(item => (*/}
+      {/*  <Card data={item} key={item.id} />*/}
+      {/*))}*/}
     </div>
-    <div className={styles.pagination}>
-      <button className={styles.button} onClick={() => getNewCards(info.prev)} disabled={!info.prev}>Prev</button>
-      <button className={styles.button} onClick={() => getNewCards(info.next)} disabled={!info.next}>Next</button>
-    </div>
+      <p>{getUA}</p>
+    {/*<div className={styles.pagination}>*/}
+    {/*  <button className={styles.button} onClick={() => getNewCards(info.prev)} disabled={!info.prev}>Prev</button>*/}
+    {/*  <button className={styles.button} onClick={() => getNewCards(info.next)} disabled={!info.next}>Next</button>*/}
+    {/*</div>*/}
     </>
   )
 }
