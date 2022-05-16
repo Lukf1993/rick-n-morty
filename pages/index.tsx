7903713bb6e4e25b64322c18ea3dd5febd4726bf
Łukasz Fundament <lukasz.fundament@iteo.com>
2022-05-16 13:46:25 +0200

    remove referer

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,6 @@ const Home: FC<IProps> = (props) => {
       <p>{getUA}</p>
       <p>{!isGoogleLoginAvailable && 'dupa1'}</p>
       <p>{isGoogleLoginAvailable && 'dupa2'}</p>
-      <p>{document.referrer}</p>
     {/*<div className={styles.pagination}>*/}
     {/*  <button className={styles.button} onClick={() => getNewCards(info.prev)} disabled={!info.prev}>Prev</button>*/}
     {/*  <button className={styles.button} onClick={() => getNewCards(info.next)} disabled={!info.next}>Next</button>*/}
